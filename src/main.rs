8e3d5525f4dd9fd40048ec3034feb15fb9035470
Luke Titley <luke@titley.com>
2021-08-17 00:12:12 +0100

    Parsing csv file seems to work

--- src/main.rs
+++ src/main.rs
@@ -8,40 +8,72 @@ use failure::{Error, Fail};
 
 #[derive(Debug, Deserialize)]
 struct Transaction {
-    type_ : String,
-    client: u32,
-    tx: u16,
-    amount: f32,
+    #[serde(rename = "type")]
+    type_: String,
+    client: u64,
+    tx: u64,
+    amount: Option<f32>,
 }
 
 #[derive(Debug, Fail)]
 enum TpError {
     #[fail(display = "invalid arguments, this take a single csv file as its argument")]
     InvalidArguments,
+
     #[fail(display = "given file does not exist {:?}", filepath)]
-    NonExistantFile {
-        filepath: std::path::PathBuf,
-    },
+    NonExistantFile { filepath: std::path::PathBuf },
+
+    #[fail(display = "IO error")]
+    IOError(std::io::Error),
+
+    #[fail(display = "csv error")]
+    CSVError(csv::Error),
 }
 
-fn parse_arguments() -> Result<PathBuf, TpError>
-{
-    let args : Vec<String> = std::env::args().collect();
+impl std::convert::From<std::io::Error> for TpError {
+    fn from(from: std::io::Error) -> Self {
+        TpError::IOError(from)
+    }
+}
+
+impl std::convert::From<csv::Error> for TpError {
+    fn from(from: csv::Error) -> Self {
+        TpError::CSVError(from)
+    }
+}
+
+fn parse_arguments() -> Result<PathBuf, TpError> {
+    let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
-        return Err(TpError::InvalidArguments{});
+        return Err(TpError::InvalidArguments {});
     }
 
     let filepath = std::path::PathBuf::from(args[1].clone());
     if !filepath.exists() {
-        return Err(TpError::NonExistantFile{filepath});
+        return Err(TpError::NonExistantFile { filepath });
     }
 
     Ok(filepath)
 }
 
-fn main() -> Result<(), Error> {
-    let filepath = parse_arguments()?;
+fn process_file(filepath: &std::path::Path) -> Result<(), TpError> {
+    // Build the CSV reader and iterate over each record.
+    let mut rdr = csv::ReaderBuilder::new()
+        .trim(csv::Trim::All)
+        .from_path(filepath)?;
+
+    for result in rdr.deserialize() {
+        let transaaction: Transaction = result?;
+        println!("{:?}", record);
+    }
 
     Ok(())
 }
 
+fn main() -> Result<(), Error> {
+    let filepath = parse_arguments()?;
+
+    process_file(&filepath)?;
+
+    Ok(())
+}
